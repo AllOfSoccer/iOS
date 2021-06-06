eccb6e3194e7e486d31229cdeeda5e1d10d07bf3
최원석 <cws653@naver.com>
2021-06-07 00:01:50 +0900

    chore: 프로젝트 셋팅

---
+++ AllOfSoccer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AllOfSoccer
+//
+//  Created by 최원석 on 2021/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
