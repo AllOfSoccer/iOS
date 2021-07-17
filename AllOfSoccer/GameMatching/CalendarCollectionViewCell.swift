8f8c78dcf4d2f77b66b5629944f77c47de4c2688
최원석 <cws653@naver.com>
2021-07-17 11:28:08 +0900

    feature: 캘린더컬렉션뷰 셀 클릭 가능하도록 구현

--- AllOfSoccer/GameMatching/CalendarCollectionViewCell.swift
+++ AllOfSoccer/GameMatching/CalendarCollectionViewCell.swift
@@ -7,8 +7,56 @@
 
 import UIKit
 
-class CalendarCollectionViewCell: UICollectionViewCell {
+protocol ViewTappedDelegate: class {
+    func viewTapped(_ cell: CalendarCollectionViewCell)
+}
+
+class CalendarCollectionViewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
+    var indexPath: IndexPath? {
+        didSet {
+            setUI()
+        }
+    }
+    var stackviewTappedBool: Bool = false
 
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+
+    @IBOutlet weak var calendarStacview: UIStackView!
+    weak var delegate: ViewTappedDelegate?
+
+    override func awakeFromNib() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        tapGesture.delegate = self
+        calendarStacview.isUserInteractionEnabled = true
+        calendarStacview.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func handleTap(recognizer:UITapGestureRecognizer) {
+        delegate?.viewTapped(self)
+        if stackviewTappedBool == false {
+            calendarStacview.backgroundColor = .blue
+            stackviewTappedBool = true
+        } else {
+            calendarStacview.backgroundColor = .clear
+            stackviewTappedBool = false
+        }
+    }
+
+    func setUI() {
+        guard let indexPath = self.indexPath else { return }
+        if indexPath.item % 7 == 6 {
+            // 일요일
+            dayLabel.textColor = .red
+            dateLabel.textColor = .red
+        } else if indexPath.item % 7 == 5 {
+            // 토요일
+            dayLabel.textColor = .blue
+            dateLabel.textColor = .blue
+        } else {
+            // 평일
+            dayLabel.textColor = .black
+            dateLabel.textColor = .black
+        }
+    }
 }
