09536eaaa6a7bb1ab70101179c71330bfa64cacc
최원석 <cws653@naver.com>
2021-08-04 21:59:18 +0900

    feature: NoticeTableView 추가

---
+++ AllOfSoccer/GameMatching/NoticeTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  NoticeTableViewCell.swift
+//  AllOfSoccer
+//
+//  Created by 최원석 on 2021/08/03.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var placeLabel: UILabel!
+    @IBOutlet private weak var contentsLabel: UILabel!
+    @IBOutlet private weak var teamImageView: UIImageView!
+    @IBOutlet private weak var teamNameLabel: UILabel!
+    @IBOutlet private weak var checkbutton: UIButton!
+    @IBOutlet private weak var recruitmentStatusLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let padding = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+        bounds = bounds.inset(by: padding)
+    }
+}
