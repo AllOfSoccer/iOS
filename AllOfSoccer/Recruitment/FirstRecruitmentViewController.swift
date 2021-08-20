a17a3af025249789cf1fc7bc0bf25a42ffd5fa25
최원석 <cws653@naver.com>
2021-08-20 14:18:11 +0900

    fix: RecruitmentView 디자인에 맞게 수정

--- AllOfSoccer/Recruitment/FirstRecruitmentViewController.swift
+++ AllOfSoccer/Recruitment/FirstRecruitmentViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class FirstRecruitmentViewController: UIViewController {
 
+    @IBOutlet private weak var recruitmentTableView: UITableView!
+    @IBOutlet private weak var addTeamButton: UIButton!
+    @IBOutlet private weak var nextButton: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.recruitmentTableView.delegate = self
+        self.recruitmentTableView.dataSource = self
+        self.recruitmentTableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+    }
+}
 
+extension FirstRecruitmentViewController: UITableViewDelegate {
+
+}
+
+extension FirstRecruitmentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecruitmentTableViewCell", for: indexPath) as? RecruitmentTableViewCell else { return UITableViewCell() }
+
+        return cell
     }
 }
