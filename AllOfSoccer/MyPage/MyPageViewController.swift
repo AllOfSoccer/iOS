3c49c0e19fb8ffc6ea334d28250a8a2eabe9fdb9
최원석 <cws653@naver.com>
2021-09-15 22:38:43 +0900

    feature: mypage 메인뷰컨트롤러 구현

--- AllOfSoccer/MyPage/MyPageViewController.swift
+++ AllOfSoccer/MyPage/MyPageViewController.swift
@@ -8,19 +8,14 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
+
+    @IBOutlet private weak var mywritingButton: UIButton!
+    @IBOutlet private weak var myfavoriteButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.mywritingButton.centerVertically(4)
+        self.myfavoriteButton.centerVertically(4)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
