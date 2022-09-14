237b6b68728aedc87d48e2b9892c1ec196b58425
조중현 <wndzlf@github.com>
2022-09-14 22:55:00 +0900

    print 문 제거

--- AllOfSoccer/GameMatching/GameMatchingViewController.swift
+++ AllOfSoccer/GameMatching/GameMatchingViewController.swift
@@ -446,7 +446,6 @@ class GameMatchingViewController: UIViewController {
 // MARK: - CollectionViewDelegate
 extension GameMatchingViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        print(indexPath.item)
         if collectionView == self.horizontalCalendarView {
             self.monthButton.setTitle(self.horizontalCalendarViewModel.makeMonthText(indexPath: indexPath), for: .normal)
         }
@@ -468,7 +467,6 @@ extension GameMatchingViewController: UICollectionViewDelegate {
             // 데이터 처리
             let deSeletedHorizontalDate = self.horizontalCalendarViewModel.getSelectedDateModel(with: indexPath).date
             self.gameMatchingModel.delete(deSeletedHorizontalDate)
-            print(self.gameMatchingModel.selectedDate)
         } else if collectionView == self.filterTagCollectionView {
             // 데이터 처리
         }
@@ -594,7 +592,6 @@ extension GameMatchingViewController: GameMatchingCalendarViewDelegate {
     }
 
     func cancelButtonDidSelected(sender: GameMatchingCalendarView) {
-        print(self.gameMatchingModel.selectedDate)
         sender.removeFromSuperview()
     }
 }
